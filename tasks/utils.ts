35363f1e8586d3ab80d9437a0655fa1ffa3d79cd
Lucas Martin Segurado <lsegurado1996@gmail.com>
2023-03-13 18:16:16 -0300

    Updated script file

---
+++ tasks/utils.ts
@@ -0,0 +1,37 @@
+import { IAttributeData } from "vscode-html-languageservice";
+
+const mdnReference = (url?: string) =>
+  url
+    ? [
+        {
+          name: "MDN Reference",
+          url,
+        },
+      ]
+    : undefined;
+export const attribute = (
+  name: string,
+  description: string,
+  values?: string[],
+) => ({
+  name,
+  description,
+  values: values?.map((x) => ({
+    name: x,
+  })),
+  references: mdnReference(
+    "https://developer.mozilla.org/en-US/docs/Web/MathML/Attribute",
+  ),
+});
+export const element = (
+  name: string,
+  description,
+  attributes: IAttributeData[] = [],
+) => ({
+  name,
+  description,
+  attributes,
+  references: mdnReference(
+    `https://developer.mozilla.org/en-US/docs/Web/MathML/Element/${name}`,
+  ),
+});
